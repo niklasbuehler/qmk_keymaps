3976ebc6d298fb7e9a6f40f39e7468f1fa2eb2ce
niklas <niklasbuehler27@gmail.com>
2018-04-29 17:26:19 +0200

    Align keys to work better with default vim navigation

--- keymap.c
+++ keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |----------------------------------------------------------------|
    * | Win   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |PgUp|
    * |----------------------------------------------------------------|
-   * |Shift(  |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift)| Up|PgDn|
+   * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /| Shift| Up|PgDn|
    * |----------------------------------------------------------------|
    * |Ctrl|Win |Alt |        Space          |Alt| FN|Ctrl|Lef|Dow|Rig |
    * `----------------------------------------------------------------'
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC,KC_TILD, \
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSLS,KC_DEL, \
   KC_LGUI,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,       KC_ENT,KC_PGUP,  \
-  KC_LSPO,     KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,     KC_RSPC,KC_UP,KC_PGDN, \
+  KC_LSFT,     KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,     KC_RSFT,KC_UP,KC_PGDN, \
   KC_LCTL,KC_LGUI,MO(_AL),                KC_SPC,                       KC_RALT,MO(_FL),KC_RCTRL, KC_LEFT,KC_DOWN,KC_RGHT),
 
   /* Keymap _FL: Function Layer
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |----------------------------------------------------------------|
    * |     |   | Up|   |   |   |   |   |   |   |   |   |   |     |    |
    * |----------------------------------------------------------------|
-   * |      |Lef|Dow|Rig|   |   |Hom|Lef|Dow| Up|Rig|End|        |    |
+   * |      |Lef|Dow|Rig|   |   |Lef|Dow| Up|Rig|   |   |        |    |
    * |----------------------------------------------------------------|
    * |        |   |   |   |   |   |   |   |   |   |   |      |   |    |
    * |----------------------------------------------------------------|
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_AL] = KEYMAP_ANSI(
   _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,  \
   _______,_______, KC_UP ,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, \
-  _______,KC_LEFT,KC_DOWN,KC_RGHT,_______,_______,KC_HOME,KC_LEFT,KC_DOWN, KC_UP ,KC_RGHT, KC_END,        _______,_______, \
+  _______,KC_LEFT,KC_DOWN,KC_RGHT,_______,_______,KC_LEFT,KC_DOWN, KC_UP ,KC_RGHT,_______,_______,        _______,_______, \
   _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,        _______,_______,_______, \
   _______,_______,_______,                    _______                    ,_______,_______,_______,_______,_______,_______)
 };
